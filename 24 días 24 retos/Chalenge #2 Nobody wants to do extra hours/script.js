d9690552760b88643addc977f5ce8c32f8560def
Luis Rodríguez <lfrz.arc@gmail.com>
2022-12-06 19:45:23 -0600

    Chalenge #2 completed

--- 24 días 24 retos/Chalenge #2 Nobody wants to do extra hours/script.js
+++ 24 días 24 retos/Chalenge #2 Nobody wants to do extra hours/script.js
@@ -2,25 +2,13 @@ const holidays = ['01/06', '04/01', '12/25']; // format mm/dd
 let count = 0;
 
 function countHours (year, holidays){
-  if(isLeapYear(year)){
-    console.log('Missing code for leap years')
-  }else{
-    holidays.forEach(date => {      
-      if(isBussinessDay(date, year)){
-        count += 2
-      }else{
-        count += 0
-      }
-    });
-  }
-}
-
-function isLeapYear(year){
-  if(year % 4 === 0){
-    return true
-  }else{
-    return false
-  }
+  holidays.forEach(date => {      
+    if(isBussinessDay(date, year)){
+      count += 2
+    }else{
+      count += 0
+    }
+  });
 }
 function isBussinessDay (holidays, year){
   const dateToString = `${holidays}/${year}`.toString()
@@ -31,4 +19,29 @@ function isBussinessDay (holidays, year){
   }else{
     return false
   }
+}
+function isLeapYear(year){
+  if(year % 4 === 0){
+    return true
+  }else{
+    return false
+  }
+}
+
+// Final solution:
+
+function countHours (year, holidays){
+  let count = 0;
+  holidays.forEach(date => {    
+    const dateToString = `${date}/${year}`.toString();
+    const dateObj = new Date(dateToString);
+    const weekday = dateObj.getDay();
+
+    if(weekday > 0 && weekday < 6){
+      count += 2
+    }else{
+      count += 0
+    }
+  });
+  return count
 }
