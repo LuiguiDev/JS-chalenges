3ef3a46528672a5e4c9c07f0f54c934ca79463b8
Luis Rodríguez <lfrz.arc@gmail.com>
2023-01-11 13:20:34 -0600

    Chalenge #22 completed!

--- Chalenge #22 La iluminacion en sintonia/script.js
+++ Chalenge #22 La iluminacion en sintonia/script.js
@@ -1,33 +1,35 @@
 const systemNames = ["house", "house", "tree_1", "tree_1", "house", "tree_2", "tree_2", "tree_3"]
 const stepNumbers = [5, 2, 1, 2, 3, 4, 5, 6]
 
-function checkStepNumbers(systemNames, stepNumbers) {
-  let status = true
-  let indexList = [];
-
-  systemNames.map(x => {  
-    let step = []
-
-    for (let i = 0; i < systemNames.length; i++) {
-      const index = systemNames.indexOf(x, i);
-      indexList[i] = index;
+function chackStepNumbers(systemNames, stepNumbers){
+  let obj = {};
+  
+  systemNames.forEach((name, index) => {
+    if(Array.isArray(obj[name])){
+      obj[name].push(stepNumbers[index])
+    }else{
+      obj[name] = [stepNumbers[index]]
     }
-    const newList = [...new Set(indexList)].filter(x => x != -1)
-    for (let i = 0; i < newList.length; i++){
-      const s = stepNumbers[newList[i]]
-      step.push(s)
-    }
-    console.log(step)
-    for (let i = 0; i < step.length - 1; i++) {
-      if(step[i] <= step[i + 1]){
-        return status
-      }else{
-        status = false
-      }
-    }
+    console.log(obj)
   })
+  for (let i = 0; i < systemNames.length; i++) {
+    const element = systemNames[i];
+    const array = obj[element]
+    
+    if(array.length > 1){
+      for (let i = 0; i < array.length - 1; i++) {
+        const element = array[i];
+        
+        if(array[i] < array[i + 1]){
+          return true
+        }else{
+          return false
+        }
+      }
+    }else{
+      return true
+    }
+  }  
+}
 
-  return status
-};
-
-console.log(checkStepNumbers(systemNames, stepNumbers))
+console.log(chackStepNumbers(systemNames, stepNumbers))
